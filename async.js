6acdf3abf4165bc6a0fa1d7ba15d16c13bb3e891
Nuriddin <ng19990107@gmail.com>
2026-05-15 00:46:13 +0500

    solving the task

---
+++ async.js
@@ -0,0 +1,89 @@
+const usersDiv = document.querySelector(".users");
+const ulEl = document.getElementById("users-list");
+const loadingEl = document.getElementById("loading");
+const info = document.querySelector(".info");
+
+const saveToLocalStorage = (data) => {
+  localStorage.setItem("users", JSON.stringify(data));
+};
+
+const getDataFromLocalStorage = () => JSON.parse(localStorage.getItem("users"));
+
+const hideLoading = () => {
+  loadingEl.style.display = "none";
+};
+
+const fetchUsersToCache = async () => {
+  const res = await fetch("./data.json");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await res.json();
+  saveToLocalStorage(data.users);
+};
+
+const removeAllUsers = () => {
+  localStorage.clear();
+  hideLoading(); // We hide usersDiv after deleting all users so we do not need to show loader we only give choice to reolad the page
+  usersDiv.classList.add("users--hidden");
+  info.classList.add("info--shown");
+};
+
+const renderListItem = ({ id, name, surname, email, age }) => {
+  const liEl = document.createElement("li");
+  liEl.innerHTML = `
+    <p>${name}</p>
+    <p>${surname}</p>
+    <p>${email}</p>
+    <p>${age}</p>
+    <button class="delete-user-btn">Delete user</button>
+  `;
+
+  liEl.querySelector(".delete-user-btn").addEventListener("click", () => {
+    const dataFromLocalStorage = getDataFromLocalStorage();
+    const filteredData = dataFromLocalStorage.filter((item) => item.id !== id);
+
+    // If we are deleting the last user so this is like we deleting all users, we clear localStorage and give a chance to reload the page to refetch users from data.json
+    if (filteredData.length === 0) {
+      removeAllUsers();
+    } else {
+      saveToLocalStorage(filteredData);
+      renderList();
+    }
+    liEl.remove();
+  });
+
+  ulEl.appendChild(liEl);
+};
+
+const renderList = () => {
+  ulEl.innerHTML = "";
+  const usersFromStorage = getDataFromLocalStorage();
+  if (usersFromStorage) {
+    usersFromStorage.forEach(renderListItem);
+    hideLoading();
+    usersDiv.classList.remove("users--hidden");
+    usersDiv
+      .querySelector(".delete-all-users")
+      .addEventListener("click", () => removeAllUsers());
+  } else {
+    setTimeout(() => {
+      fetchUsersToCache()
+        .then(() => getDataFromLocalStorage())
+        .then(() => renderList())
+        .catch((error) => {
+          hideLoading();
+          const errorEl = document.createElement("div");
+          errorEl.classList.add("error");
+          errorEl.innerHTML = `
+            <span>${error} :(</span>
+            <button onclick="window.location.reload()">reload the page</button>
+          `;
+          document.body.appendChild(errorEl);
+        });
+    }, 3000);
+  }
+};
+
+renderList();
